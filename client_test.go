7c75e6aba60c477fd648721f3eca94488e25c39c
orvice L <orvice@gmail.com>
2025-02-16 23:17:17 +0800

    update test

--- client_test.go
+++ client_test.go
@@ -1,11 +1,15 @@
 package proto
 
 import (
-	"reflect"
+	"context"
+	"os"
 	"testing"
+
+	pb "github.com/icebase/mu/proto/v1"
 )
 
 func TestClient(t *testing.T) {
-	c := NewClient("127.0.0.1:10000")
-	t.Log(reflect.TypeOf(c))
+	c := NewClient(os.Getenv("MU_ADDR"), os.Getenv("MU_TOKEN"))
+	resp, err := c.GetUsers(context.Background(), &pb.GetUsersRequest{})
+	t.Log(resp, err)
 }
